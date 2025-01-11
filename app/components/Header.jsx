3b290614a6b04aed88fc9d4a6eb3f065267b9ab2
Sao <Vipin.sao@ssipmt.com>
2025-01-11 19:48:42 +0530

    added images

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
         whileInView={{ scale: 1 }}
         transition={{ duration: 0.8, type: "spring", stifness: "100" }}
       >
-        <Image src={assets.profile_img} alt="" className="rounded-full w-32" />
+        <Image src={assets.user_image} alt="" className="rounded-full w-32" />
       </motion.div>
       <motion.h3
         initial={{ y: -20, opacity: 0 }}
--- assets/assets.js
+++ assets/assets.js
@@ -36,6 +36,7 @@ import send_icon from "./send-icon.png";
 import right_arrow_bold from "./right-arrow-bold.png";
 import right_arrow_bold_dark from "./right-arrow-bold-dark.png";
 
+
 export const assets = {
   user_image,
   code_icon,
@@ -74,6 +75,7 @@ export const assets = {
   send_icon,
   right_arrow_bold,
   right_arrow_bold_dark,
+  user-main,
 };
 
 export const workData = [
